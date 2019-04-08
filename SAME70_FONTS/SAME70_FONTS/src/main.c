1171f67a97925a245c628f22ac5caa4e08a4f4c7
VictorLMoura <victorlaperutademoura@hotmail.com>
2019-04-08 15:42:31 -0300

    agora usando rtt

--- SAME70_FONTS/SAME70_FONTS/src/main.c
+++ SAME70_FONTS/SAME70_FONTS/src/main.c
@@ -19,7 +19,6 @@
 struct ili9488_opt_t g_ili9488_display_opt;
 
 void BUT_init(void);
-void TC_init(Tc * TC, int ID_TC, int TC_CHANNEL, int freq);
 void configure_lcd(void);
 void font_draw_text(tFont *font, const char *text, int x, int y, int spacing);
 
@@ -52,6 +51,48 @@ void BUT_init(void){
 	NVIC_EnableIRQ(BUT_PIO_ID);
 	NVIC_SetPriority(BUT_PIO_ID, 1);
 };
+
+void RTT_Handler(void)
+{
+	uint32_t ul_status;
+
+	/* Get RTT status */
+	ul_status = rtt_get_status(RTT);
+
+	/* IRQ due to Time has changed */
+	if ((ul_status & RTT_SR_RTTINC) == RTT_SR_RTTINC) {  }
+
+	/* IRQ due to Alarm */
+	if ((ul_status & RTT_SR_ALMS) == RTT_SR_ALMS) {
+		pin_toggle(LED_PIO, LED_IDX_MASK);    // BLINK Led
+		f_rtt_alarme = true;                  // flag RTT alarme
+	}
+}
+
+static float get_time_rtt(){
+	uint ul_previous_time = rtt_read_timer_value(RTT);
+}
+
+static void RTT_init(uint16_t pllPreScale, uint32_t IrqNPulses)
+{
+	uint32_t ul_previous_time;
+
+	/* Configure RTT for a 1 second tick interrupt */
+	rtt_sel_source(RTT, false);
+	rtt_init(RTT, pllPreScale);
+	
+	ul_previous_time = rtt_read_timer_value(RTT);
+	while (ul_previous_time == rtt_read_timer_value(RTT));
+	
+	rtt_write_alarm_time(RTT, IrqNPulses+ul_previous_time);
+
+	/* Enable RTT interrupt */
+	NVIC_DisableIRQ(RTT_IRQn);
+	NVIC_ClearPendingIRQ(RTT_IRQn);
+	NVIC_SetPriority(RTT_IRQn, 0);
+	NVIC_EnableIRQ(RTT_IRQn);
+	rtt_enable_interrupt(RTT, RTT_MR_ALMIEN);
+}
 
 void configure_lcd(void){
 	/* Initialize display parameter */
@@ -87,8 +128,8 @@ int main(void) {
 	sysclk_init();	
 	configure_lcd();
 	
- 	font_draw_text(&sourcecodepro_28, "OIMUNDO", 50, 50, 1);
- 	font_draw_text(&calibri_36, "Oi Mundo! #$!@", 50, 100, 1);
+ 	//font_draw_text(&sourcecodepro_28, "OIMUNDO", 50, 50, 1);
+ 	//font_draw_text(&calibri_36, "Oi Mundo! #$!@", 50, 100, 1);
  	font_draw_text(&arial_72, "102456", 50, 200, 2);
 
 	/* Disable the watchdog */
@@ -98,11 +139,49 @@ int main(void) {
 
 	/* Configura os botões */
 	BUT_init();
-
-	/** Configura timer TC0, canal 1 */
-	TC_init(TC0, ID_TC0, 0, 1);
-		
+	
+	// Inicializa RTT com IRQ no alarme.
+	f_rtt_alarme = true;
+  
 	while(1) {
 		pmc_sleep(SAM_PM_SMODE_SLEEP_WFI);
+		
+			if (f_rtt_alarme){
+      
+				/*
+				* O clock base do RTT é 32678Hz
+				* Para gerar outra base de tempo é necessário
+				* usar o PLL pre scale, que divide o clock base.
+				*
+				* Nesse exemplo, estamos operando com um clock base
+				* de pllPreScale = 32768/32768/2 = 2Hz
+				*
+				* Quanto maior a frequência maior a resolução, porém
+				* menor o tempo máximo que conseguimos contar.
+				*
+				* Podemos configurar uma IRQ para acontecer quando 
+				* o contador do RTT atingir um determinado valor
+				* aqui usamos o irqRTTvalue para isso.
+				* 
+				* Nesse exemplo o irqRTTvalue = 8, causando uma
+				* interrupção a cada 2 segundos (lembre que usamos o 
+				* pllPreScale, cada incremento do RTT leva 500ms (2Hz).
+				*/
+				uint16_t pllPreScale = (int) (((float) 32768) / 2.0);
+				uint32_t irqRTTvalue  = 4;
+      
+				// reinicia RTT para gerar um novo IRQ
+				RTT_init(pllPreScale, irqRTTvalue);         
+      
+				/*
+				* caso queira ler o valor atual do RTT, basta usar a funcao
+				*   rtt_read_timer_value()
+				*/
+      
+				/*
+				* CLEAR FLAG
+				*/
+				f_rtt_alarme = false;
+			}
 	}
 }
